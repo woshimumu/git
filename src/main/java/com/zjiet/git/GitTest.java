b822c4c69f6959a6be7f70da2c41463c797cc2d6
woshimumu <90259338+woshimumu@users.noreply.github.com>
2022-09-27 15:50:12 +0800

    Update GitTest.java

--- src/main/java/com/zjiet/git/GitTest.java
+++ src/main/java/com/zjiet/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix git test");
         System.out.println("push test");
         System.out.println("push test1");
+        System.out.println("clone test");
 
     }
 }
