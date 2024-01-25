6cae518ee2720936f1170daf755c5a85c26d2b94
woshimumu <90259338+woshimumu@users.noreply.github.com>
2024-01-26 00:01:16 +0800

    Update GitTest.java

--- src/main/java/com/zjiet/git/GitTest.java
+++ src/main/java/com/zjiet/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push test1");
         System.out.println("clone test");
-
+        System.out.println("clone test1111");
     }
 }
